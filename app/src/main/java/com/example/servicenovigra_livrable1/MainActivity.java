5ba507d17412d93f1bf1380c9f58749f426ad1b6
mayssatbr <64826384+mayssatbe@users.noreply.github.com>
2020-10-18 03:29:37 -0400

    bouton login_customer et l'activité Welcome_cutomer

--- app/src/main/java/com/example/servicenovigra_livrable1/MainActivity.java
+++ app/src/main/java/com/example/servicenovigra_livrable1/MainActivity.java
@@ -2,22 +2,46 @@ package com.example.servicenovigra_livrable1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     EditText userName,password;
     Button btnLogin_client;
-    Button btLogin_employee;
+    Button btnLogin_employee;
+    Button  btnSign_up;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        userName=(EditText)findViewById(et_usernameL);
-        password=(EditText)findViewById(et_password);
-        btLogin_employee=(EditText)findViewById(clientbt);
-        btnLogin_client=(EditText)findViewById(employeebt);
+        userName=(EditText)findViewById(R.id.et_usernameL);
+        password=(EditText)findViewById(R.id.et_password);
+        btnSign_up=(EditText)findViewById(R.id.bt_signin);
+
+        btnLogin_client=(EditText)findViewById(R.id.customerbt);
+
+
+        btnLogin_client.setOnClickListener(new View.OnClickListener()){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(getApplicationContext(),Welcome_cutomer.class);
+                startActivity(intent);
+            }
+        }
+
+        btnSign_up.setOnClickListener(new View.OnClickListener()){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(getApplicationContext(),Sign_up.class);
+                startActivity(intent);
+            }
+        }
+
+
+
     }
 
 }
---
+++ app/src/main/java/com/example/servicenovigra_livrable1/Welcome_cutomer.java
@@ -0,0 +1,14 @@
+package com.example.servicenovigra_livrable1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Welcome_cutomer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_cutomer);
+    }
+}
